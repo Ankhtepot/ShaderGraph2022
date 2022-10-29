889f9be7d5daef147c01a9d0a382e6ad67c08ef4
ZavodnyPetr <petr.zavodny@sensearena.com>
2022-10-29 07:28:32 +0200

    Up to lesson 26.

---
+++ Assets/S05 Spaces/DebugVertices.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DebugVertices : MonoBehaviour {
+	private Mesh _mesh;
+	private Vector3[] _vertices;
+
+	void OnDrawGizmos()
+	{
+		if (_vertices == null)
+		{
+			_mesh = GetComponent<MeshFilter>().sharedMesh;
+			_vertices = _mesh.vertices;
+		}
+		foreach (Vector3 v in _vertices)
+            Handles.Label(transform.position + v, $"v: {v}");
+	}
+}
